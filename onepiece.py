8541d201bef13762c4bcf772dabae346eadfc498
temiloluwaseun <precioustutu14@gmail.com>
2022-10-10 07:58:53 +0100

    i added onepiece

--- onepiece.py
+++ onepiece.py
@@ -1,5 +1,14 @@
 anime=input("Favourite anime character")
 
-anime2=anime.replace("a"," ").replace("e","0").replace("i","1").replace("o","2").replace("u","3")
+def a():
+    return anime.replace("a"," ")
+def e():
+    return a().replace("e","0")
+def i():
+    return e().replace("i","1")
+def o():
+    return i().replace("o","2")
+def u():
+    return o().replace("u","3")
 
-print(anime2)
+print(u())
